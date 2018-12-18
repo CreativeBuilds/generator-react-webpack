d612fd0dd6ccf56928facca19d7bd18387e50743
CreativeBuilds <gaminggonecreative@gmail.com>
2018-12-18 11:49:21 -0500

    back to 15

--- generators/component/templates/3/components/StatefulNoStyles.js
+++ generators/component/templates/3/components/StatefulNoStyles.js
@@ -1,14 +1,16 @@
+
 'use strict';
 
 import React from 'react';
-import { useState } from 'react';
 
-let <%= component.className %> = () => {
-  return (
-    <div className="<%= style.className %>">
-      Please edit <%= component.path %>/<%= component.fileName %> to update this component 9!
-    </div>
-  );
+class <%= component.className %> extends React.Component {
+  render() {
+    return (
+      <div className="<%= style.className %>">
+        Please edit <%= component.path %>/<%= component.fileName %> to update this component 7!
+      </div>
+    );
+  }
 }
 
 <%= component.className %>.displayName = '<%= component.displayName %>';
@@ -18,28 +20,3 @@ let <%= component.className %> = () => {
 // <%= component.className %>.defaultProps = {};
 
 export default <%= component.className %>;
-
-/**
- * OLD VERSION FOR REACT 15 IF NEEDED
- */
-// 'use strict';
-
-// import React from 'react';
-
-// class <%= component.className %> extends React.Component {
-//   render() {
-//     return (
-//       <div className="<%= style.className %>">
-//         Please edit <%= component.path %>/<%= component.fileName %> to update this component 7!
-//       </div>
-//     );
-//   }
-// }
-
-// <%= component.className %>.displayName = '<%= component.displayName %>';
-
-// // Uncomment properties you need
-// // <%= component.className %>.propTypes = {};
-// // <%= component.className %>.defaultProps = {};
-
-// export default <%= component.className %>;
--- generators/component/templates/3/components/StatefulWithStyles.js
+++ generators/component/templates/3/components/StatefulWithStyles.js
@@ -1,15 +1,18 @@
 'use strict';
 
 import React from 'react';
-import { useState } from 'react';
 
 require('./<%= component.displayName.replace("Component","") %>.scss');
-
-let <%= component.className %> = () => {
-  return <div className="<%= style.className %>">
-    Please edit <%= component.path %><%= component.fileName %> to update this component 8!
-  </div>
-};
+class <%= component.className %> extends React.Component {
+  render() {
+    return (
+      <div className="<%= style.className %>">
+        
+        Please edit <%= component.path %>/<%= component.fileName %> to update this component 8!
+      </div>
+    );
+  }
+}
 
 <%= component.className %>.displayName = '<%= component.displayName %>';
 
@@ -18,31 +21,3 @@ let <%= component.className %> = () => {
 // <%= component.className %>.defaultProps = {};
 
 export default <%= component.className %>;
-
-
-/**
- * OLD VERSION FOR REACT 15 IF NEEDED
- */
-// 'use strict';
-
-// import React from 'react';
-
-// require('./<%= component.displayName.replace("Component","") %>.scss');
-// class <%= component.className %> extends React.Component {
-//   render() {
-//     return (
-//       <div className="<%= style.className %>">
-        
-//         Please edit <%= component.path %>/<%= component.fileName %> to update this component 8!
-//       </div>
-//     );
-//   }
-// }
-
-// <%= component.className %>.displayName = '<%= component.displayName %>';
-
-// // Uncomment properties you need
-// // <%= component.className %>.propTypes = {};
-// // <%= component.className %>.defaultProps = {};
-
-// export default <%= component.className %>;
